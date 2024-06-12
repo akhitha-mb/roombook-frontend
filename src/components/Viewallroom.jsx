9f5719d432c2c9fea655f05aa2feae434dd2f7df
akhitha-mb <akhiakhitha3@gmail.com>
2024-06-13 01:17:40 +0530

    created

---
+++ src/components/Viewallroom.jsx
@@ -0,0 +1,59 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+export const Viewallroom = () => {
+ 
+        const [data,changeData] = useState([])
+    const fetchData = () => {
+        axios.get("http://localhost:8083/viewall").then(
+            (response) => {
+                changeData(response.data)
+            }
+        ).catch().finally()
+    }
+    useEffect(()=>{fetchData()},[])
+  return (
+    <div>
+        
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">HOTEL NAME</th>
+      <th scope="col">DATE</th>
+      <th scope="col">CHECK-IN</th>
+      <th scope="col">CHECK-OUT</th>
+
+    </tr>
+  </thead>
+  <tbody>
+   {data.map(
+    (value,index)=>{
+        return <tr>
+
+      <td>{value.hotelname}</td>
+      <td>{value.date}</td>
+      <td>{value.checkin}</td>
+      <td>{value.checkout}</td>
+   
+        </tr>
+    }
+   )
+   }
+    
+  </tbody>
+</table>
+                </div>
+            </div>
+        </div>
+        
+        </div>
+  )
+}
+
+       
+        
+        
+      
